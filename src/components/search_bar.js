cb37d1a439a503c9e7206c7d0e06f1d4ef656104
Enmanuel Medina <enmanuelm19@gmail.com>
2018-01-17 20:49:13 -0300

    Adding class component, use state and update it

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
 
-const SearchBar = () => {
-  return <input />;
+class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = { term: '' };
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+        Value: {this.state.term}
+      </div>
+    );
+  }
 }
 //To be able call the component in other files
 export default SearchBar;
