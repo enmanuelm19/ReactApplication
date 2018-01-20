3ca58f595bc715e4607990cb1466f97122a4d108
Enmanuel Medina <enmanuelm19@gmail.com>
2018-01-20 17:34:25 -0300

    Adding component and without selected video

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyCUh11u_IPlbssF8Y-X1ExTOcWg5CtPRVg';
+const API_KEY = 'AIzaSyA85iw2pEIFouNyPsFkPQLJVH-CEd7wTUE';
 
-//Create a new component, This component should produce html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
+
+
+
+
 
 // react take component and put it on the page
 ReactDOM.render(<App />, document.querySelector('.container'));
